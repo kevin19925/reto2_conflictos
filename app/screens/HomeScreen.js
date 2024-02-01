fc3620264a28c7ae2b4df68206a6015473f6ce48
kevin19925 <kevinqui1_@hotmail.com>
2024-01-31 21:16:11 -0500

    mensaje st2

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,52 +1,49 @@
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet, Button } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export const Home = ({ navigation }) => {
-    return <View style={styles.container}>
-        <Text>HOME</Text>
-
-        <View style={styles.botones}>
-
-            <View style={styles.boton1}>
-                <Button
-                    title='CONTACTS'
-                    onPress={() => {
-                        navigation.navigate('ContactsNav');
-                    }}
-                />
-            </View>
-
-
-
-            <Button
-                title='PRODUCTOS'
-                onPress={() => {
-                    navigation.navigate('ProductNav');
-                }}
-            />
-
+  return (
+    <View style={styles.container}>
+      <Text>BIENVENIDO A MI APLICACION , SOY KEVIN </Text>
+      <Text>ESTAMOS RULAY ssss </Text>
+      <View style={styles.botones}>
+        <View style={styles.boto1}>
+          <Button
+            color="red"
+            title="CONTACTS"
+            onPress={() => {
+              navigation.navigate("ContactsNav");
+            }}
+          />
         </View>
 
+        <Button
+          title="PRODUCTOS"
+          onPress={() => {
+            navigation.navigate("ProductNav");
+          }}
+        />
+      </View>
     </View>
-
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    botones: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        padding: 10,
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  botones: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 10,
+  },
+  boton1: {
+    marginRight: 60,
+  },
 
-    },
-    boton1: {
-
-        marginRight: 60,
-
-    },
+  Btn: {},
 });
