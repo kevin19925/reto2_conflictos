a423b968193f4746341e4315a0a4dd2264ed9c1f
EstebanSanchez2701 <eim.sanchezbaez@gmail.com>
2024-01-31 21:46:57 -0500

    Paso Contact estudiante1

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 
       <Stack.Navigator>
         <Stack.Screen name='WellcomeEsteban_Sanchez' component={Home} />
-        <Stack.Screen name='ContactsNav' component={Contacts} />
+        <Stack.Screen name='CallNumber' component={Contacts} />
         <Stack.Screen name='ProductNavigation' component={Product}/>
 
 
